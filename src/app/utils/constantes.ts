d1e9cc7500b7c0d96cacbd1fc214870d9498c81f
Alberto Rey <areymoreno@outlook.es>
2024-09-20 14:00:31 +0200

    [UPDATE] - Add new podcast colab.

--- src/app/utils/constantes.ts
+++ src/app/utils/constantes.ts
@@ -6,27 +6,32 @@ export class Constantes {
   static readonly CLIENT_ID = "";
   static readonly BEARER_FINAL = "Bearer ";
 
-  static readonly CLIENT_ID_TELEGRAM = "";
-  static readonly BEARER_TELEGRAM = "Bearer ";
-
   static readonly STAR_GAMES = [
     "2XKO",
+    "Anno 117: Pax Romana",
+    "Ace Attorney Investigations Collection",
     "Alone in the Dark",
+    "Assassin's Creed Shadows",
     "Avowed",
     "Apollo Justice: Ace Attorney Trilogy",
     "Banishers: Ghosts of New Eden",
+    "Batman: Arkham Shadow",
     "Beyond Good & Evil - 20th Anniversary Edition",
     "Brothers: A Tale of Two Sons",
+    "Call of Duty: Black Ops 6",
     "Death Stranding 2: On The Beach",
     "Deep Rock Galactic: Survivor",
     "Destiny 2: The Final Shape",
     "Dragon Ball: Sparking! Zero",
+    "Dragon Age: The Veilguard",
     "Dragon's Dogma II",
+    "Doom: The Dark Ages",
+    "EA Sports FC 25",
     "Eastward: Octopia!",
     "Eiyuden Chronicle: Hundred Heroes",
     "Elden Ring: Shadow of the Erdtree",
+    "Epic Mickey: Rebrushed",
     "Final Fantasy VII Rebirth",
-    "Final Fantasy XIV: Dawntrail",
     "Final Fantasy XVI: The Rising Tide",
     "Frostpunk 2",
     "Grand Theft Auto VI",
@@ -34,42 +39,73 @@ export class Constantes {
     "Homeworld 3",
     "Indiana Jones and the Great Circle",
     "Inazuma Eleven: Victory Road",
+    "Just Dance VR",
+    "Just Dance 2025 Edition",
+    "LEGO Horizon Adventures",
     "Like a Dragon: Infinite Wealth",
+    "Little Nightmares III",
     "Luigi's Mansion 2 HD",
+    "Mafia: The Old Country",
     "Manor Lords",
     "Mario vs. Donkey Kong",
+    "Mario & Luigi: Brothership",
     "Marvel 1943: Rise of Hydra",
     "Metaphor: ReFantazio",
+    "Metal Gear Solid Delta: Snake Eater",
     "Nightingale",
+    "Paper Mario: The Thousand-Year Door",
     "Persona 3 Reload",
-    "Persona 3 Reload: Episode Aigis",
+    "Pokémon Legends: Z-A",
     "Prince of Persia: The Lost Crown",
     "Prison Architect 2",
     "Professor Layton and the New World of Steam",
     "Rise of the Ronin",
     "Sand Land",
     "Senua's Saga: Hellblade II",
+    "Sid Meier's Civilization VII",
     "Shin Megami Tensei V: Vengeance",
     "S.T.A.L.K.E.R. 2: Heart of Chornobyl",
-    "Star Wars: Outlaws",
+    "Star Wars Outlaws",
+    "Space Marine VR - Defenders of Avarax",
+    "Stellar Blade",
     "Suicide Squad: Kill the Justice League",
     "Suikoden I & II HD Remaster: Gate Rune and Dunan Unification Wars",
     "Tekken 8",
+    "The Legend of Zelda: Echoes of Wisdom",
+    "The Plucky Squire",
+    "The Rogue Prince of Persia",
     "The Wolf Among Us 2",
     "Tom Clancy's The Division: Resurgence",
     "Tomb Raider I•II•III Remastered",
     "Unicorn Overlord",
+    "Until Dawn",
     "Vampire: The Masquerade - Bloodlines 2",
     "Visions of Mana",
     "Warhammer 40,000: Space Marine II",
-    "World of Warcraft: The War Within"];
+    "Warhammer 40,000: Rogue Trader - Void Shadows"];
 
   static readonly SEASON_GAMES = [
     ""
   ]
 
   static readonly PATCH_GAMES = [
-    ""
+    "Alan Wake II: Night Springs",
+    "Diablo IV: Vessel of Hatred",
+    "Fallout 4: Next-Gen Update",
+    "Guild Wars 2: Janthir Wilds",
+    "V Rising: Legacy of Castlevania",
+    "Vampire Survivors: Operation Guns",
+    "Persona 3 Reload: Episode Aigis",
+    "Final Fantasy XIV: Dawntrail",
+    "The Crew: Motorfest - Season 5",
+    "World of Warcraft: The War Within"
+  ]
+
+  static readonly PREMIUM_PACK = [
+    "Ace Combat 7: Skies Unknown Deluxe Edition",
+    "Assassin's Creed Shadows: Thrown to the Dogs",
+    "Persona 3 Reload: Velvet Costume & BGM Set",
+    "V Rising: Legacy of Castlevania - Premium Pack"
   ]
 
   static readonly MONTHS_YEAR = [
@@ -84,7 +120,13 @@ export class Constantes {
     { opcion: 'Septiembre', valor: 9, append: 'SEP' },
     { opcion: 'Octubre', valor: 10, append: 'OCT' },
     { opcion: 'Noviembre', valor: 11, append: 'NOV' },
-    { opcion: 'Diciembre', valor: 12, append: 'DIC' }
+    { opcion: 'Diciembre', valor: 12, append: 'DIC' },
+  ]
+
+  static readonly GAMES_YEAR_TBA = [
+    { opcion: 'Q2 2024', valor: 1, append: 'Q2TBA'},
+    { opcion: 'Q3 2024', valor: 2, append: 'Q3TBA'},
+    { opcion: 'Q4 2024', valor: 3, append: 'Q4TBA'}
   ]
 
   static readonly MONTHS_YEAR_TBA = [
